5c82d8197dec15e50f35ebb67324e959eec094d0
thukyaw11 <helloeleven968@gmail.com>
2021-01-10 01:56:51 +0630

    feat: e warranty initiated

--- src/containers/_nav.js
+++ src/containers/_nav.js
@@ -1,55 +1,61 @@
 export default [{
-  _name: 'CSidebarNav',
-  _children: [{
-      _name: 'CSidebarNavItem',
-      name: 'Dashboard',
-      to: '/dashboard',
-      icon: 'cil-speedometer',
-      // badge: {
-      //   color: 'primary',
-      //   text: 'NEW'
-      // }
-    },
-    {
-      _name: 'CSidebarNavTitle',
-      _children: ['TABLES']
-    },
-    {
-      _name: 'CSidebarNavItem',
-      name: 'Orders',
-      to: '/orders',
-      icon: 'cil-tags'
-    },
-    {
-      _name: 'CSidebarNavItem',
-      name: 'Products',
-      to: '/products',
-      icon: 'cil-layers'
-    },
-    {
-      _name: 'CSidebarNavTitle',
-      _children: ['PAGES']
-    },
-    {
-      _name: 'CSidebarNavDropdown',
-      name: 'Page Options',
-      route: '/base',
-      icon: 'cil-settings',
-      items: [{
-          name: 'Promotions',
-          to: '/promotions'
+    _name: 'CSidebarNav',
+    _children: [{
+            _name: 'CSidebarNavItem',
+            name: 'Dashboard',
+            to: '/dashboard',
+            icon: 'cil-speedometer',
+            // badge: {
+            //   color: 'primary',
+            //   text: 'NEW'
+            // }
         },
         {
-          name: 'FAQS',
-          to: '/faqs'
+            _name: 'CSidebarNavTitle',
+            _children: ['TABLES']
         },
         {
-          name: 'NewPromotion',
-          to: '/new/promotion'
+            _name: 'CSidebarNavItem',
+            name: 'Orders',
+            to: '/orders',
+            icon: 'cil-tags'
         },
-      
-      ]
-    },
+        {
+            _name: 'CSidebarNavItem',
+            name: 'Products',
+            to: '/products',
+            icon: 'cil-layers'
+        },
+        {
+            _name: 'CSidebarNavItem',
+            name: 'eWarranty',
+            to: '/products',
+            icon: 'cil-layers'
+        },
+        {
+            _name: 'CSidebarNavTitle',
+            _children: ['PAGES']
+        },
+        {
+            _name: 'CSidebarNavDropdown',
+            name: 'Page Options',
+            route: '/base',
+            icon: 'cil-settings',
+            items: [{
+                    name: 'Promotions',
+                    to: '/promotions'
+                },
+                {
+                    name: 'FAQS',
+                    to: '/faqs'
+                },
+                {
+                    name: 'NewPromotion',
+                    to: '/new/promotion'
+                },
 
-  ]
+            ]
+        },
+
+    ]
 }]
